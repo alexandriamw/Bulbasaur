902535c21cd2175393a9b0f02012fdbc6347fa20
CMK <ckrnke@gmail.com>
2019-08-15 14:20:01 -0500

    added script for maps

---
+++ assets/javascript/script.js
@@ -0,0 +1,4 @@
+// this is our google maps api key and link
+
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAvh-RJE3-FnbTJlwKg-npCYZl_Yo8P6RU&callback=initMap"
+type="text/javascript"></script>
