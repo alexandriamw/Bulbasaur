4f1839f0e46e625170fa8f3b08f2af4473e324e1
Adam Cooper <frankidyfrank3@gmal.com>
2019-08-31 05:56:45 -0500

    Added: right sidebar "console data" this just displays the info we are already storing in local forage to the screen to help fill out the blank areas
    Added: more info stored in the issArray local forage Array because it is already storing an object why not keep all of the data together in one spot
    Added: the html that was previously hidden when the hamburger menu was closed into the javascript so to not mess with the new "console feature" and vise versa
    
    ToDo: style the console data div's and make them look more presentable

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -11,10 +11,25 @@ let map;
 let lat_global = "";
 let lon_global = "";
 
+//the distance global variables to save to local forage because the data is already being processed so why not save it for display
+let distance_global = "";
+let city_cords_global = {
+    lat: 44.948628,
+    lon: -93.245329,
+    city: "Minneapolis"
+}
+
 let lat_WeatherGlobal = "";
 let lon_WeatherGlobal = "";
 
+//store the interval for displaying data in the right bar normally when the menu is not present as to be able to start and stop it
+let rightBarDataGlobal = {
+    timerInterval: null,
+    isRunning: false
+}
+
 getLastPoints();
+displayRightBarData();
 
 
 
@@ -101,15 +116,20 @@ function getIssPosition(callbackFunction) {
 
 // the loop that pushes lat and lon to localForage
 setInterval(() => {
-    //combined the localTime aray and issArray so we do not have to mess with 2 arrays to get the time 
+    //combined the localTime array and issArray so we do not have to mess with 2 arrays to get the time 
     let time = moment().format('MMMM Do YYYY, h:mm:ss a');
     localforage.getItem("issArray").then(function (results) {
         let issData = results || [];
-        issData.push({
+        issData.unshift({//changed from push to unshift so newest will always be at the top 
             lat: lat_global,
             lon: lon_global,
-            time: time
+            time: time,
+            distance: distance_global,
+            cityData: city_cords_global
         });
+
+        // this keeps the data from being stored more that 100 items/ always keeps the newer data
+        if(issData.length > 100) issData.pop();
         localforage.setItem("issArray", issData).then(function () {
 
         });
@@ -130,12 +150,17 @@ function getLastPoints() {
 
 
 
+//grab the right bar and put it into a variable
+const rightBar = document.getElementById("mainInput");
+
 //menu animation with anime.js
 const menuElement = document.getElementById("menu");
 menuElement.addEventListener("click", function () {
 
 
     if (menuElement.classList.contains("open")) {
+        rightBar.innerHTML = "";
+        displayRightBarData();
         anime({
             targets: "div#menu",
             rotate: {
@@ -195,6 +220,8 @@ menuElement.addEventListener("click", function () {
         });
 
         menuElement.classList.add("open");
+        loadRight();
+        displayRightBarData();
     }
 });
 
@@ -203,8 +230,8 @@ menuElement.addEventListener("click", function () {
 
 
 function distanceMath() {
-    //placeholder coords are Minneapolis
-    (calcCrow(44.948628, -93.245329, lat_global, lon_global).toFixed(1));
+    //placeholder coords are Minneapolis -  Now in global variable to be changed upon input
+    (calcCrow(city_cords_global.lat, city_cords_global.lon, lat_global, lon_global).toFixed(1));
     //This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
     function calcCrow(lat1, lon1, lat2, lon2) {
         var R = 6371; // km
@@ -218,6 +245,7 @@ function distanceMath() {
         var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
         var d = R * c;
         document.getElementById("issDistance").innerHTML = Math.floor(d) + " Kilometers";
+        distance_global = Math.floor(d) + " Kilometers";
         return d;
     }
 
@@ -237,18 +265,102 @@ setInterval(() => {
 
 // was lazy and just hid the fields until clicked
 function loadRight() {
-    document.getElementById("menu").addEventListener("click", () => {
+    rightBar.innerHTML = "";
 
-        let inputField = document.getElementsByClassName("testFields");
-        console.log(inputField)
-        for (let i = 0; i < inputField.length; i++) {
-            if (inputField[i].style.display === "none") {
-                inputField[i].style.display = "block";
+    //adds the animation delay in dynamically so as to not have to bind an id to this just for that 
+    let animateDelay = 1000;
+
+    //loop thorught because i am lazy and did not want to copy and paste 4 times until we figure out what we are doing here
+    for (let i = 1; i<5; i++){
+        //create a div with an h3 of test inside of it just to see if it
+        const newTestDiv = createDivs();
+        newTestDiv.innerHTML = `<h3>test</h3>`;
+        newTestDiv.style = `animation-delay: ${animateDelay}ms`;
+        rightBar.prepend(newTestDiv);
+
+        //devreases the animation delay as we are prepending to the right side and we want the top of the menu to load first
+        animateDelay -= 250;
+    }
+
+    //create an input field and add it to the top of the right bar 
+    const newInputDiv = createDivs();
+    newInputDiv.innerHTML = `<input id="toggledField" type="text" value="" name="inputValue">`;
+    rightBar.prepend(newInputDiv);
+}
+
+//this function exists to create div's for the right sidebar and add a preset class list
+function createDivs(){
+    const newDiv = document.createElement("div");
+    newDiv.classList = "animated fadeInRightBig testFields";
+    return newDiv; 
+}
+
+
+//intended to display sort of a console-esk log of previous coordinates every 30 seconds if the interval is running 
+function displayRightBarData(){
+    if(rightBarDataGlobal.isRunning === false){
+        rightBarDataGlobal.timerInterval = setInterval(()=>{
+            createRightConsoleData();
+        },30000);
+        //wait 2 seconds after function called to display data because it looks cooler
+        setTimeout(createRightConsoleData,2000)
+
+        //set is running var to true so we can identify if the interval is running or not
+        rightBarDataGlobal.isRunning = true;
+    } else {
+        //clear the interval so to not mess wit the menu when open
+        clearTimeout(rightBarDataGlobal.timerInterval)
+        //set is running var to false so we can identify if the interval is running or not
+        rightBarDataGlobal.isRunning = false
+    }
+}
+
+//this creates the "Console like" elements in the right bar using the data stored in local forage
+function createRightConsoleData(){
+    //query local forage for the issArray array
+    localforage.getItem("issArray").then(function (results) {
+        let issData = results || [];
+
+        //if the array is not empty do things
+        if (issData.length !== 0){
+
+            //check if previous data is displayed
+            let previousConsoleData = document.getElementsByClassName("consoleData");
+
+            //if not then try to make some exist in a reverse for loop counting down from 10
+            if(previousConsoleData.length === 0){
+                for(let i = 10; i>=0;i--){
+                    //if issData with the index of i exists then put it on the page
+                    if (issData[i].lat !== undefined){
+                        //create a div for it add consoleData to the classList so to be identified
+                        let newDiv = createDivs()
+                        newDiv.classList.add("consoleData");
+
+                        //set the id of the new div to the longitude coordinate and set the innerHTML to the data
+                        newDiv.id = `${issData[i].lon}`
+                        newDiv.innerHTML = `<p style="font-size: 14px">lat: ${issData[i].lat}<br/>lon: ${issData[i].lon}<br/>timeStamp: ${issData[i].time}<br/>distance from ${issData[i].cityData.city}: ${issData[i].distance}</p>`;
+                        rightBar.prepend(newDiv);
+                    }
+                }
             } else {
-                inputField[i].style.display = "none";
+                
+                //if  then try to make some exist in a reverse for loop counting down from 10
+                for(let i = 10; i>=0;i--){
+                    let existing = document.getElementById(`${issData[i].lon}`);
+                    console.log(existing);
+                    if (existing === null && issData[i].lat !== undefined){
+                        //create a div for it add consoleData to the classList so to be identified
+                        let newDiv = createDivs()
+                        newDiv.classList.add("consoleData");
+
+                        //set the id of the new div to the longitude coordinate and set the innerHTML to the data
+                        newDiv.id = `${issData[i].lon}`
+                        newDiv.innerHTML = `<p style="font-size: 14px">lat: ${issData[i].lat}<br/>lon: ${issData[i].lon}<br/>timeStamp: ${issData[i].time}<br/>distance from ${issData[i].cityData.city}: ${issData[i].distance}</p>`;
+                        rightBar.prepend(newDiv);
+                    }
+                }
+
             }
         }
-
     })
 }
-loadRight();
